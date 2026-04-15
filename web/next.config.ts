916f038b4f1523a35eb8396e29f7e40178d16c8e
PomeloFruit <alan.jiabaodu@gmail.com>
2026-04-14 22:17:18 -0400

    chore: document intentional wildcard hostname in remotePatterns

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        // News articles come from many different source domains so we allow
+        // any HTTPS host. TODO: restrict to known news CDN domains before production.
         protocol: 'https',
         hostname: '**',
         pathname: '/**',
